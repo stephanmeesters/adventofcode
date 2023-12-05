761e22174495b54ff5a76c5d28b37013e04c97e8
Stephan Meesters <stephan.meesters@gmail.com>
2023-12-05 16:18:01 +0100

    day4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -4,16 +4,16 @@ use std::fs;
 fn main() {
     let re = Regex::new(r"\d+").unwrap();
 
-    let contents = fs::read_to_string("input2.txt").unwrap();
+    let contents = fs::read_to_string("input.txt").unwrap();
     let lines: Vec<&str> = contents.lines().collect();
 
-    let mut points: Vec<usize> = vec![0; lines.len()];
     let mut num_cards: Vec<usize> = vec![0; lines.len()];
+    let mut total_score = 0;
 
     for ind in 0..lines.len() {
         let line = lines[ind];
         let parts: Vec<&str> = line.split("|").collect();
-        assert!(parts.len() == 2);
+        assert_eq!(parts.len(), 2);
 
         let winning_part = parts[0].split(":").last().unwrap();
         let our_part = parts[1];
@@ -27,28 +27,35 @@ fn main() {
             .filter_map(|d| d.as_str().parse::<usize>().ok())
             .collect();
 
+        let mut num_matches: usize = 0;
         let mut score: usize = 0;
         for num in &our_numbers {
             if winning_numbers.contains(num) {
-                score += 1;
+                num_matches += 1;
+                if score == 0 {
+                    score = 1;
+                } else {
+                    score = score + score;
+                }
             }
         }
-        points[ind] = score;
-        for oind in ind..(ind + score + 1).min(lines.len()) {
-            if oind == ind && num_cards[ind] == 0 {
-                num_cards[ind] = 1;
+
+        for oind in ind..(ind + num_matches + 1).min(lines.len()) {
+            if oind == ind {
+                num_cards[ind] += 1;
                 continue;
             }
             num_cards[oind] += num_cards[ind];
         }
+
+        total_score += score;
     }
 
-    println!("{:?}", num_cards);
-
-    let mut total_score = 0;
+    let mut total_num_cards = 0;
     for ind in 0..lines.len() {
-        total_score += num_cards[ind];
+        total_num_cards += num_cards[ind];
     }
 
     println!("total score: {}", total_score);
+    println!("total num cards: {}", total_num_cards);
 }
