378542fbf4f17ff3e49000a71d1857e4e7405074
Stephan Meesters <stephan@stephan.home>
2023-12-24 14:34:50 +0100

    day10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,34 +1,36 @@
 use std::fs;
 
+macro_rules! getchar{
+    ($a:expr,$b:expr)=>{
+        {
+$a[$b.x].chars().nth($b.y).unwrap()
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Pos {
     x: usize,
     y: usize,
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Map {
-    Visited(usize),
-    Unseen,
-    Wall,
-}
-
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
     let lines: Vec<&str> = contents.lines().collect();
-    let mut map: Vec<Vec<Map>> = vec![vec![Map::Unseen; lines.first().unwrap().len()]; lines.len()];
 
-    let start_index: Pos = get_start_index(lines).unwrap();
+    let start_index: Pos = get_start_index(&lines).unwrap();
+    println!("{:?}", start_index);
 
     let mut cur_index: &Pos = &start_index;
     loop {
-
+        let cur_str = getchar!(lines, cur_index);
+        println!("{}", cur_str);
+        break;
     }
 
-    println!("{:?}", start_index);
 }
 
-fn get_start_index(lines: Vec<&str>) -> Option<Pos> {
+fn get_start_index(lines: &Vec<&str>) -> Option<Pos> {
     for i in 0..lines.len() {
         let line = lines[i];
         if let Some(pos) = line.find('S') {
@@ -36,4 +38,34 @@ fn get_start_index(lines: Vec<&str>) -> Option<Pos> {
         }
     }
     None
+}
+
+fn get_neighbors(lines: &Vec<&str>, index: Pos) -> (Pos, Pos) {
+        let cur = getchar!(lines, index);
+match char {
+    'S' => {
+        let n1: Pos;
+        let n2: Pos;
+
+    },
+    '|' => return (Pos { x: index.x, y: index.y - 1} , Pos { x: index.x ,y:index.y + 1}),
+
+}
+}
+
+fn get_chars_around(lines: &Vec<&str>, index: Pos) -> Vec<Pos> {
+    let neigh:Vec<Pos> = Vec::new();
+    if index.x - 1 >= 0 {
+        neigh.push(Pos { x: index.x - 1, y: index.y });
+    }
+    if index.x + 1 < lines[0].len() {
+        neigh.push(Pos { x: index.x - 1, y: index.y });
+    }
+    if index.y - 1 >= 0 {
+        neigh.push(Pos { x: index.x, y: index.y - 1 });
+    }
+    if index.y + 1 < lines.len() {
+        neigh.push(Pos { x: index.x, y: index.y + 1 });
+    }
+    neigh
 }
